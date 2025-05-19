4908cbefeb58600487dde13da76e043bbc278f53
Junichi Suzuki <yunichee@outlook.jp>
2025-05-19 12:10:39 +0000

    Prettier/ESLint導入

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,32 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  { files: ["**/*.{ts,tsx}"] },
+  { ignores: ["**/generated/**/*", "**/components/ui/**/*"] },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    // eslint-plugin-importに関する設定
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: ["builtin", "external", "internal"],
+          alphabetize: { order: "asc", caseInsensitive: true },
+          "newlines-between": "always", // import groups 1行空ける
+          pathGroups: [
+            { pattern: "src/components/**", group: "internal", position: "before" },
+            { pattern: "src/lib/**", group: "internal", position: "before" },
+          ],
+        },
+      ],
+      "import/newline-after-import": "error",
+      "import/no-duplicates": "error",
+    },
+  },
+  eslintConfigPrettier, // Prettierとの競合防止
 ];
 
 export default eslintConfig;
